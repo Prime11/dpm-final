2943e683e22b10bb0eb6732f0984cc8674006e68
kellymorrison <kelly.morrison@mail.mcgill.ca>
2013-11-26 16:59:46 -0500

    turned class into a thread

--- src/Detection.java
+++ src/Detection.java
@@ -2,34 +2,40 @@ import lejos.nxt.ColorSensor;
 import lejos.nxt.SensorPort;
 
 
-public class Detection {
+public class Detection extends Thread {
 	private final int COLOR_THRESHOLD = 278; //verify this threshold 
 	private int numberOfBlocks = 0; 
-	private boolean hasThreeBlocks = false; 
 	final ColorSensor blockDetector;
 	
 	public Detection(ColorSensor colorSensor) {
 		this.blockDetector = colorSensor; 
 	}
 	
-	//when this method returns true, the robot should drive to the respective dropoff zone 
-	public boolean obtainedBlock() {
-		while (numberOfBlocks < 3) {
+	public void run() {
+		while(true) {
 			if (blockDetector.getNormalizedLightValue() > COLOR_THRESHOLD) { //if there is a block in front of the color sensor
 				/*waits 4 seconds so block can pass (might need to edit sleep time) 
 				 * this ensures that block is not accounted for twice
 				 */
+				numberOfBlocks++;
 				try { 
-					Thread.sleep(4000);
+					Thread.sleep(4000); //test the amount of time it sleeps 
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					//does nothing 
 				}
-				numberOfBlocks++; 
-				return hasThreeBlocks; 
 			}
 		}
-		hasThreeBlocks = true; 
-		return hasThreeBlocks; 
+	}
+	
+	public boolean hasThreeBlocks() {
+		if (numberOfBlocks == 3) {
+			return true; 
+		}
+		return false; 
+	}
+
+	public void resetNumberOfBlocks() {
+		numberOfBlocks = 0; 
 	}
 }
